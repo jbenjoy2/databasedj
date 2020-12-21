142e8dd2907e83c75cfd4888375883a39d6cd922
Jason Benjoya <jasonbenjoya@gmail.com>
2020-12-21 15:28:36 -0600

    added relations to models and built them; adjusted base template a bit

--- playlist-app/models.py
+++ playlist-app/models.py
@@ -8,22 +8,38 @@ db = SQLAlchemy()
 class Playlist(db.Model):
     """Playlist."""
 
+    __tablename__ = 'playlists'
+
     # ADD THE NECESSARY CODE HERE
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
 
 
 class Song(db.Model):
     """Song."""
-
+    __tablename__ = 'songs'
     # ADD THE NECESSARY CODE HERE
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.Text, nullable=False)
+    artist = db.Column(db.Text, nullable=False, default='Unknown Artist')
+
+    playlist = db.relationship(
+        'Playlist', secondary='playlists_songs', backref='songs')
 
 
 class PlaylistSong(db.Model):
     """Mapping of a playlist to a song."""
 
+    __tablename__ = 'playlists_songs'
     # ADD THE NECESSARY CODE HERE
-
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    playlist_id = db.Column(db.Integer, db.ForeignKey('playlists.id'))
+    song_id = db.Column(db.Integer, db.ForeignKey('songs.id'))
 
 # DO NOT MODIFY THIS FUNCTION
+
+
 def connect_db(app):
     """Connect to database."""
 
